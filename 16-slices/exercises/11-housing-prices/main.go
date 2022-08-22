4285e14150c652b5aa624d9d959d93e4c2fbf33d
Rémy Machavoine <remygt@hotmail.fr>
2022-08-22 11:05:45 +0200

    add complete housing prices

--- 16-slices/exercises/11-housing-prices/main.go
+++ 16-slices/exercises/11-housing-prices/main.go
@@ -8,6 +8,12 @@
 
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 // ---------------------------------------------------------
 // EXERCISE: Housing Prices
 //
@@ -76,4 +82,43 @@ Istanbul,500,10,5,1000000`
 
 		separator = ","
 	)
+
+	var (
+		locs                       []string
+		sizes, beds, baths, prices []int
+	)
+
+	rows := strings.Split(data, "\n")
+
+	for _, row := range rows {
+		cols := strings.Split(row, separator)
+
+		locs = append(locs, cols[0])
+
+		n, _ := strconv.Atoi(cols[1])
+		sizes = append(sizes, n)
+
+		n, _ = strconv.Atoi(cols[2])
+		beds = append(beds, n)
+
+		n, _ = strconv.Atoi(cols[3])
+		baths = append(baths, n)
+
+		n, _ = strconv.Atoi(cols[4])
+		prices = append(prices, n)
+	}
+
+	for _, h := range strings.Split(header, separator) {
+		fmt.Printf("%-15s", h)
+	}
+	fmt.Printf("\n%s\n", strings.Repeat("=", 75))
+
+	for i := range rows {
+		fmt.Printf("%-15s", locs[i])
+		fmt.Printf("%-15d", sizes[i])
+		fmt.Printf("%-15d", beds[i])
+		fmt.Printf("%-15d", baths[i])
+		fmt.Printf("%-15d", prices[i])
+		fmt.Println()
+	}
 }
